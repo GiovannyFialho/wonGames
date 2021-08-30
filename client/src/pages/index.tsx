55760a22cb07cbf0ba56eb6371e6875afa7c82f1
GiovannyFialho <giovannyf@outlook.com>
2021-08-30 19:08:42 -0300

    criacao da rota de banners e implementacao no client

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,15 +3,36 @@ import Home, { HomeTemplateProps } from "templates/Home";
 import bannerMock from "components/BannerSlider/mock";
 import gamesMock from "components/GamecardSlider/mock";
 import highlightMock from "components/Highlight/mock";
+import { initializeApollo } from "utils/apollo";
+import { QueryHome } from "graphql/generated/QueryHome";
+import { QUERY_HOME } from "graphql/queries/home";
 
 export default function Index(props: HomeTemplateProps) {
     return <Home {...props} />;
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+    const apolloClient = initializeApollo();
+
+    const { data } = await apolloClient.query<QueryHome>({
+        query: QUERY_HOME
+    });
+
     return {
         props: {
-            banners: bannerMock,
+            revalidate: 60,
+            banners: data.banners.map((banner) => ({
+                img: `http://localhost:1337${banner.image?.url}`,
+                title: banner.title,
+                subtitle: banner.subtitle,
+                buttonLabel: banner.button?.label,
+                buttonLink: banner.button?.link,
+                ...(banner.ribbon && {
+                    ribbon: banner.ribbon.text,
+                    ribbonSize: banner.ribbon.size,
+                    ribbonColor: banner.ribbon.color
+                })
+            })),
             newGames: gamesMock,
             mostPopularHighlight: highlightMock,
             mostPopularGames: gamesMock,
