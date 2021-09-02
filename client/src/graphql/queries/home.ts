89f73db2a5f81495665d64ea99ef6ffd6f75019b
GiovannyFialho <giovannyf@outlook.com>
2021-09-01 21:57:42 -0300

    criacao de fragmets para banner e game e criacao de querys com filtros para secoes da home

--- client/src/graphql/queries/home.ts
+++ client/src/graphql/queries/home.ts
@@ -1,22 +1,34 @@
 import { gql } from "@apollo/client";
+import { BannerFragment } from "graphql/fragments/banner";
+import { GameFragment } from "graphql/fragments/game";
 
 export const QUERY_HOME = gql`
     query QueryHome {
         banners {
-            image {
-                url
-            }
-            title
-            subtitle
-            button {
-                link
-                label
-            }
-            ribbon {
-                text
-                color
-                size
-            }
+            ...BannerFragment
+        }
+
+        newGames: games(
+            where: { release_date_lte: "2021-09-01" }
+            sort: "release_date:desc"
+            limit: 5
+        ) {
+            ...GameFragment
+        }
+
+        upcomingGames: games(
+            where: { release_date_gt: "2021-09-01" }
+            sort: "release_date:asc"
+            limit: 5
+        ) {
+            ...GameFragment
+        }
+
+        freeGames: games(where: { price: 0 }, sort: "release_date:desc") {
+            ...GameFragment
         }
     }
+
+    ${BannerFragment}
+    ${GameFragment}
 `;
