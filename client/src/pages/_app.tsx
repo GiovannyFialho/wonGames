2b4673d7df605f47be4aff66abd5cfcb5c1ccd6e
GiovannyFialho <giovannyf@outlook.com>
2022-01-06 19:50:27 -0300

    adicionando progressbar na aplicacao

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,14 @@
+import NextNprogress from "nextjs-progressbar";
 import { ApolloProvider } from "@apollo/client";
+import { ThemeProvider } from "styled-components";
+import { CartProvider } from "hooks/use-cart";
+
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { ThemeProvider } from "styled-components";
-import { useApollo } from "utils/apollo";
-import { CartProvider } from "hooks/use-cart";
 
 import GlobalStyles from "styles/global";
 import theme from "styles/theme";
+import { useApollo } from "utils/apollo";
 
 function App({ Component, pageProps }: AppProps) {
     const client = useApollo(pageProps.initialApolloState);
@@ -26,6 +28,13 @@ function App({ Component, pageProps }: AppProps) {
                         <link rel="manifest" href="/manifest.json" />
                     </Head>
                     <GlobalStyles />
+                    <NextNprogress
+                        color="#F231A5"
+                        startPosition={0.3}
+                        stopDelayMs={200}
+                        height={5}
+                        showOnShallow={true}
+                    />
                     <Component {...pageProps} />
                 </CartProvider>
             </ThemeProvider>
