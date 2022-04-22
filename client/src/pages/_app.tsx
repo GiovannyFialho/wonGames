7562ee793c3d3914973d1ab37a10a2a2e935a409
Giovanny Fialho <giovannyf@outlook.com>
2022-04-22 14:30:46 -0300

    criacao da estrutura do use-wisthlist e adicionando possibilidade de pegar a session no client side

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,16 +1,17 @@
+import type { AppProps } from "next/app";
+import Head from "next/head";
 import NextNprogress from "nextjs-progressbar";
 
 import { Provider as AuthProvider } from "next-auth/client";
 import { ApolloProvider } from "@apollo/client";
 import { ThemeProvider } from "styled-components";
 import { CartProvider } from "hooks/use-cart";
+import { WishlistProvider } from "hooks/use-wishlist";
 
-import type { AppProps } from "next/app";
-import Head from "next/head";
+import { useApollo } from "utils/apollo";
 
 import GlobalStyles from "styles/global";
 import theme from "styles/theme";
-import { useApollo } from "utils/apollo";
 
 function App({ Component, pageProps }: AppProps) {
     const client = useApollo(pageProps.initialApolloState);
@@ -20,31 +21,33 @@ function App({ Component, pageProps }: AppProps) {
             <ApolloProvider client={client}>
                 <ThemeProvider theme={theme}>
                     <CartProvider>
-                        <Head>
-                            <title>Won Games</title>
-                            <link
-                                rel="shortcut icon"
-                                href="/img/icon-192.png"
+                        <WishlistProvider>
+                            <Head>
+                                <title>Won Games</title>
+                                <link
+                                    rel="shortcut icon"
+                                    href="/img/icon-192.png"
+                                />
+                                <link
+                                    rel="apple-touch-icon"
+                                    href="/img/icon-512.png"
+                                />
+                                <meta
+                                    name="description"
+                                    content="The best Game Store in the world!"
+                                />
+                                <link rel="manifest" href="/manifest.json" />
+                            </Head>
+                            <GlobalStyles />
+                            <NextNprogress
+                                color="#F231A5"
+                                startPosition={0.3}
+                                stopDelayMs={200}
+                                height={5}
+                                showOnShallow={true}
                             />
-                            <link
-                                rel="apple-touch-icon"
-                                href="/img/icon-512.png"
-                            />
-                            <meta
-                                name="description"
-                                content="The best Game Store in the world!"
-                            />
-                            <link rel="manifest" href="/manifest.json" />
-                        </Head>
-                        <GlobalStyles />
-                        <NextNprogress
-                            color="#F231A5"
-                            startPosition={0.3}
-                            stopDelayMs={200}
-                            height={5}
-                            showOnShallow={true}
-                        />
-                        <Component {...pageProps} />
+                            <Component {...pageProps} />
+                        </WishlistProvider>
                     </CartProvider>
                 </ThemeProvider>
             </ApolloProvider>
