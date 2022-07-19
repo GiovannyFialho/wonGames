e5cba40f533ae0763d2634ed179e2f602b610bd5
Giovanny Fialho <giovannyf@outlook.com>
2022-07-19 19:50:36 -0300

    atualizando test-utils

--- client/src/utils/test-utils.tsx
+++ client/src/utils/test-utils.tsx
@@ -3,27 +3,38 @@ import { render, RenderOptions } from "@testing-library/react";
 import { ThemeProvider } from "styled-components";
 
 import theme from "styles/theme";
+
 import {
     CartContext,
     CartContextData,
     CartContextDefaultValues
 } from "hooks/use-cart";
 
+import {
+    WishlistContext,
+    WishlistContextData,
+    WishlistContextDefaultValues
+} from "hooks/use-wishlist";
+
 type CustomRenderProps = {
     cartProviderProps?: CartContextData;
+    wishlistProviderProps?: WishlistContextData;
 } & Omit<RenderOptions, "queries">;
 
 const customRender = (
     ui: ReactElement,
     {
         cartProviderProps = CartContextDefaultValues,
+        wishlistProviderProps = WishlistContextDefaultValues,
         ...renderOptions
     }: CustomRenderProps = {}
 ) =>
     render(
         <ThemeProvider theme={theme}>
             <CartContext.Provider value={cartProviderProps}>
-                {ui}
+                <WishlistContext.Provider value={wishlistProviderProps}>
+                    {ui}
+                </WishlistContext.Provider>
             </CartContext.Provider>
         </ThemeProvider>,
         renderOptions
